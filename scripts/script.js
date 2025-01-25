e21d390a4ea7ca83e68e127b181220d72c8e5553
Seb <everydayseb@pm.me>
2025-01-25 15:47:48 +0000

    Create function for computer choice

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-console.log("Hello, world");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    let rpsString = "";
+
+    switch (choice) {
+        case 0:
+            rpsString = "rock";
+            break;
+        case 1:
+            rpsString = "paper";
+            break;
+        case 2:
+            rpsString = "scissors";
+            break;
+        default:
+            console.log("Computer error choosing RPS.");
+    }
+
+    return rpsString;
+}
+
+for (let i = 0; i < 25; i++) {
+    console.log(getComputerChoice());
+}
