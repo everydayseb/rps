70720bfdada32edbd1c18fe42937359d49640051
Seb <everydayseb@pm.me>
2025-01-27 23:38:10 +0000

    Moved global variables to top

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     let rpsString = "";
@@ -23,6 +26,3 @@ function getHumanChoice() {
     let humanChoice = prompt("Enter your choice (rock/paper/scissors)");
     return humanChoice.toLowerCase();
 }
-
-let humanScore = 0;
-let computerScore = 0;
